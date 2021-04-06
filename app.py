182ed842804c5b6fe20d030258e8b097f5d6d1b9
SINA <sinasoheili79@gmail.com>
2021-04-06 23:33:52 +0430

    implement function to register new user

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, abort
-from database_config import DB_SERVER, DB_USER, DB_PASSWD, DB_NAME, TABLE_SHOP, TABLE_SELLER
+from database_config import DB_SERVER, DB_USER, DB_PASSWD, DB_NAME, TABLE_SHOP, TABLE_SELLER, TABLE_USER
 import mysql.connector
 
 app = Flask(__name__)
@@ -128,6 +128,42 @@ def get_seller_info():
         return {"find":True , "seller":{"id":id, "name":name, "last_name":last_name , "phone":phone}}
     else:
         abort(500)
+
+@app.route('/register_user' , methods=['GET'])
+def registar_user():
+    name = request.args.get('name', None)
+    last_name = request.args.get('last_name', None)
+    phone = request.args.get('phone', None)
+    passwd = request.args.get('passwd', None)
+
+    if name==None or last_name==None or phone==None or passwd==None:
+        abort(400)
+
+    #TODO: chek phone is valid
+    #TODO: encrypt passwd
+
+    cmd = f"INSERT INTO {TABLE_USER} (name, last_name, phone, passwd) VALUES (%s, %s, %s, %s)"
+    params = (name, last_name, phone, passwd)
+
+    db = connect_to_database()
+    if db==None:
+        abort(500)
+    cursor = db.cursor()
+    cursor.execute(cmd , params)
+    try:
+        db.commit()
+        
+        cmd = f"SELECT * FROM {TABLE_USER} WHERE phone={phone}"
+        cursor.execute(cmd)
+        result = cursor.fetchall()
+        if len(result) != 1:
+            abort(500)
+        else :
+            id = result[0][0]
+            return {"id":id, "name":name , "last_name":last_name , "phone":phone , "passwd":passwd}
+            
+    except e as Exception:
+        abort(503)   
 
 if __name__ == "__main__":
     app.run("localhost" , 5000 , True)
