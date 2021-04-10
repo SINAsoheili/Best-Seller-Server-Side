673aff64e35b23935b714742226acc0a244b54bd
SINA <sinasoheili79@gmail.com>
2021-04-10 22:50:49 +0430

    fix confilict of merge gitlab and github

--- app.py
+++ app.py
@@ -83,12 +83,7 @@ def registar_shop():
         result = cursor.fetchone()
         id = result[0]
         return {"id":id, "name":name, "address":address, "latitude":latitude, "longitude":longitude, "id_seller":id_seller, "id_category":id_category, "site":site , "description":description, "phone":phone}
-            
-<<<<<<< HEAD
-    except:
-=======
     except :
->>>>>>> e2415c1237e6b2e8ae6488b745926a2f7f59f276
         abort(503)    
 
 
